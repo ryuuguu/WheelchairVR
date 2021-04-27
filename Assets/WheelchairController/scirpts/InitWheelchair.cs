79bb45a453baccc6f04fd132300f0ce67cd87311
ryuuguu <grant@ryuuguu.com>
2021-04-27 16:02:35 +0900

    adding wheelchair vehicle to WheelChair Screen Control scene

--- Assets/WheelchairController/scirpts/InitWheelchair.cs
+++ Assets/WheelchairController/scirpts/InitWheelchair.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class InitWheelchair : MonoBehaviour
 {
-
     public Transform cameraOffset;
     public Transform headCamera;
-    
-   
     public void setForwardAngle() {
         cameraOffset.eulerAngles += new Vector3(0, -1*headCamera.eulerAngles.y, 0);
         gameObject.SetActive(false);
     }
-
-    
 }
